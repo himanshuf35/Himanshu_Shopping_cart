0f7505dfc75c58b70007034d915ed606fcf7d404
himanshuf25 <himanshuf22@hotmail.com>
2018-07-30 00:16:09 +0530

    save changes

--- Himanshu_Grocery/Himanshu_Shopping_cart.js
+++ Himanshu_Grocery/Himanshu_Shopping_cart.js
@@ -1,4 +1,3 @@
-console.log("this is working:")
 var Fruits = [{
         name: "Mango",
         MRP: 25,
@@ -17,7 +16,7 @@ var Fruits = [{
     {
         name: "Guava",
         MRP: 40,
-        category: "Fruit,"
+        category: "Fruit"
     },
     {
         name: "Banana",
@@ -43,7 +42,7 @@ var Fruits = [{
         name: "Pomegranate",
         MRP: 250,
         category: "Fruit"
-    },
+    }
 ];
 
 var Vegetables = [{
@@ -87,10 +86,10 @@ var Vegetables = [{
         category: "Vegetable"
     },
     {
-        name: "tomato ",
+        name: "tomato",
         MRP: 20,
         category: "Vegetable"
-    },
+    }
 ];
 
 var Dairy_Product = [{
@@ -132,7 +131,7 @@ var Dairy_Product = [{
         name: "Cheese-Cubes",
         MRP: 110,
         category: "Dairy"
-    },
+    }
 
 ];
 
@@ -175,14 +174,14 @@ var Biscuits = [{
         name: "50-50",
         MRP: 50,
         category: "Biscuit"
-    },
+    }
 
 ];
 var Products = [Fruits, Vegetables, Dairy_Product, Biscuits];
-console.log(Products);
-var search_table=[];
-var cart_table=[];
+var search_table = [];
+var cart_table = [];
 
+//function to search into available items
 function Search_Products() {
     let text_to_search = document.getElementById("search_text").value;
     search_table = [];
@@ -192,128 +191,136 @@ function Search_Products() {
         Products.forEach((categor) => {
             categor.forEach((product) => {
                 if (regex.test(product.name)) {
-                    console.log(product.name);
+
                     search_table.push(product);
                 }
             });
         });
-        if(search_table.length!=0)
-        {
-            
-        let text_data = "<table> <tr> <th>Name</th> <th>MRP</th> <th>Category</th> <th>No. Of Items</th> </tr>";
-        search_table.forEach((found_product) => {
-            //let ID=found_product.name;
-            text_data = text_data + "<tr> <td>" + found_product.name + "</td> <td> " + found_product.MRP + "</td> <td>" + found_product.category + "</td> <td>  <input type='number'min='0' max='8' id="+found_product.name+"> </td> </tr>";
-        });
-        text_data = text_data + "</table>";
-        document.getElementById("dynamic").innerHTML = text_data;
-    }
-    else
-    {
-        document.getElementById("dynamic").innerHTML = "<p>cannot find anything<p>";
-    }
+        if (search_table.length != 0) {
+
+            let text_data = "<table> <tr> <th>Name</th> <th>MRP</th> <th>Category</th> <th>No. Of Items</th> </tr>";
+            search_table.forEach((found_product) => {
+
+                text_data = text_data + "<tr> <td>" + found_product.name + "</td> <td> " + found_product.MRP + "</td> <td>" + found_product.category + "</td> <td>  <input type='number'min='0' max='8' id=" + found_product.name + "> </td> </tr>";
+            });
+            text_data = text_data + "</table>";
+            document.getElementById("dynamic").innerHTML = text_data;
+        } else {
+            document.getElementById("dynamic").innerHTML = "<p>cannot find anything<p>";
+        }
 
     }
 }
 
+//reset function
 
 function reset() {
-    search_table=[];
-    document.getElementById("dynamic").innerHTML="";
-    document.getElementById("search_text").value="";
-    //console.log(search_table); 
+    search_table = [];
+    document.getElementById("dynamic").innerHTML = "";
+    document.getElementById("search_text").value = "";
+
 }
 
-function add_to_cart()
-{
-    console.log(search_table.length);
-    if(search_table.length>0)
-    {
-        
-        //let text_data="";
-    search_table.forEach((item_to_add) => 
-    {
-        
-        let num=document.getElementById(item_to_add.name).value;
-        //let integer = parseInt(num);
-        if(num>0)
-        {
-            let bool=true;
-            cart_table.forEach((check_exist_item) => 
-            {
-                if(check_exist_item.Name === item_to_add.name)
-                {
-                    bool=false;
-                    check_exist_item.Quantity=num- -check_exist_item.Quantity;
-                    //break;
+//function to add selected item to cart
+function add_to_cart() {
+
+    if (search_table.length > 0) {
+
+
+        search_table.forEach((item_to_add) => {
+
+
+            let num = document.getElementById(item_to_add.name).value;
+
+            if (num > 0) {
+                let bool = true;
+                cart_table.forEach((check_exist_item) => {
+                    if (check_exist_item.Name === item_to_add.name) {
+                        bool = false;
+                        check_exist_item.Quantity = num - -check_exist_item.Quantity;
+
+                    }
+                });
+                if (bool) {
+                    cart_table.push({
+                        Name: item_to_add.name,
+                        Quantity: num,
+                        Category: item_to_add.category,
+                        MRP: item_to_add.MRP
+                    });
                 }
-            }
-        );
-        if(bool)
-        {
-            cart_table.push({Name:item_to_add.name,Quantity:num,Category:item_to_add.category,MRP:item_to_add.MRP});
-        }
-                
-        }
-        console.log(num);
-    });
 
-    if(cart_table.length>=0)
-    {
-       
-       
-            let text_data = "<table> <tr> <th>Name</th> <th>Category</th> <th>MRP</th> <th>Quantity</th>  <th>Price</th></tr>";
-            cart_table.forEach((cart_product) => {
-            //total_amount=total_amount+price;
-            //let ID=found_product.name;
-            let price=(cart_product.MRP)*(cart_product.Quantity);
-            text_data = text_data + "<tr> <td>" + cart_product.Name + "</td> <td> " + cart_product.Category + "</td> <td>" +cart_product.MRP+"</td> <td>" +cart_product.Quantity+ "</td> <td>"+price+ "</td> </tr>";
-            
-            console.log(price);
+            }
+
         });
-        text_data = text_data + "</table>";
-        //console.log(total_amount);
-        document.getElementById("Table").innerHTML=text_data;
-       // 
-        
-    }
-    //document.getElementById("Tbody").innerHTML = text_data;
-    console.log(cart_table);
-    }
-    else
-    {
-         document.getElementById("contact").innerHTML="<p>Please search and select any items first</p>"
+
+        if (cart_table.length > 0) {
+
+
+            let text_data = "<table> <tr> <th>Name</th> <th>Quantity</th> </tr>";
+            cart_table.forEach((cart_product) => {
+
+
+
+                text_data = text_data + "<tr> <td>" + cart_product.Name + "</td><td>" + cart_product.Quantity + "</td> </tr>";
+
+
+            });
+            text_data = text_data + "</table>";
+
+            document.getElementById("Table").innerHTML = text_data;
+
+
+        } else {
+            alert("Please Select something first!");
+        }
+
+
+    } else {
+        alert("Please enter something to search");
     }
 
 
 }
 
-function checkout()
-{
-    if(cart_table.length>=0)
-    {
-        let total_amount=0;
-        
-            //let text_data = "<table> <tr> <th>Name</th> <th>Category</th> <th>MRP</th> <th>Quantity</th>  <th>Price</th></tr>";
-            cart_table.forEach((cart_product) => {
-         let price=(cart_product.MRP)*(cart_product.Quantity);
-            total_amount=total_amount+price;
-            //let ID=found_product.name;
-            
-            console.log(price);
-        });
-       // text_data = text_data + "</table>";
-        //console.log(total_amount);
-        document.getElementById("Table").innerHTML="";
-        document.getElementById("static3").innerHTML="Total Amount="+total_amount+""
-       
-        
-       // 
-        
+//functions for checkout
+
+function checkout() {
+    if (cart_table.length > 0) {
+
+        sessionStorage.setItem("cart_table", JSON.stringify(cart_table));
+
+
+
+        document.getElementById("Table").innerHTML = "";
+        window.open("Link_page.html");
+
+
+
+
+
+    } else {
+        alert("Cart is empty!");
     }
-    else
-    {
-        document.getElementById("column2").innerHTML="<p>Please search and select any items first</p>"
-    }
-   cart_table=[];
+    cart_table = [];
+}
+
+function checkout2() {
+    var cart_table = JSON.parse(sessionStorage.cart_table);
+    let total_amount = 0;
+
+    let text_data = "<table> <tr> <th>Name</th> <th>Category</th> <th>MRP</th> <th>Quantity</th>  <th>Price</th></tr>";
+    cart_table.forEach((cart_product) => {
+        let price = (cart_product.MRP) * (cart_product.Quantity);
+        total_amount = total_amount + price;
+        text_data = text_data + "<tr> <td>" + cart_product.Name + "</td> <td> " + cart_product.Category + "</td> <td>" + cart_product.MRP + "</td> <td>" + cart_product.Quantity + "</td> <td>" + price + "</td> </tr>";
+
+
+
+    });
+    text_data = text_data + "</table>";
+    text_data=text_data+"<div id='bill'><b>Total Amount="+total_amount+"</b></div>"
+    document.getElementById("second").innerHTML = text_data;
+    sessionStorage.clear();
+
 }
